57930743590468b69fca25f269d993c3eb40524d
yun seok <str17gogo@naver.com>
2023-04-19 13:21:49 +0900

    feat: 마지막 크롤 시간 작업1 - crawlDate 도메인 추가

---
+++ src/main/java/com/example/javaproject2/CrawlDate.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2;
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+    // alt + insert
+
+    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+}
+
