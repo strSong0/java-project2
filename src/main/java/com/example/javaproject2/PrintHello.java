901d6726b743b01853f35e490459266173261c5d
yun seok <str17gogo@naver.com>
2023-04-19 11:25:30 +0900

    first commit

---
+++ src/main/java/com/example/javaproject2/PrintHello.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2;
+
+public class PrintHello {
+    public static void main(String[] args) {
+    }
+    public void print(){
+        System.out.println("hello");
+    }
+}
