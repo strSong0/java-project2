ebe6c82247f388a92c0323ceaf69850e294c2361
yun seok <str17gogo@naver.com>
2023-04-19 14:51:56 +0900

    feat: BufferReader 추가 - 줄단위 읽어오기 가능

---
+++ src/main/java/com/example/javaproject2/week1/BufferedReaderTest.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week1;
+
+import java.io.IOException;
+
+public class BufferedReaderTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReaderEx bufferedReaderEx = new BufferedReaderEx();
+        bufferedReaderEx.readALine();
+
+    }
+}
