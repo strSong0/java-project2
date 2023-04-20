f307b2e4344d7858c6499a592f87f33afb333b2a
yun seok <str17gogo@naver.com>
2023-04-20 17:06:03 +0900

    feat: Codeup update - codup 1015

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1015.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float fval = sc.nextFloat();
+
+        System.out.printf("%.2f", fval);
+    }
+}
